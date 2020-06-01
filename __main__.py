0f9fe0811c27bdafd394c1b6a37df646c3800a25
henkwang <attapon_srem@hotmail.com>
2020-06-01 12:27:42 +0700

    init Version 1.

---
+++ __main__.py
@@ -0,0 +1,5 @@
+#!./venv/bin/python
+from src.app import create_app
+
+if __name__ == "__main__":
+    create_app()
---
+++ init.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+chmod +x __main__.py
+
+echo "Python version."
+python3 -V
+
+echo "which python3"
+which python3
+
+if [ ! -d "venv/" ] 
+then
+    echo "Create Python Virtual Envelopment." 
+    python3 -m virtualenv venv
+else
+    echo "Python Virtual Envelopment is exist."
+fi
+
+echo "Init python virtual envelopment"
+source venv/bin/activate
+
+if [ ! -f "venv/bin/py" ] 
+then
+    echo "alise python3 => py"
+    cp "$(which python3)" venv/bin/py
+fi
+which py
+
+if [ -f "requirements.txt" ] 
+then
+    echo "Install Python Module." 
+    python3 -m pip install -U pip
+    python3 -m pip install -r requirements.txt
+else
+    echo "No file requirements.txt"
+fi
+
+echo "Init end."
